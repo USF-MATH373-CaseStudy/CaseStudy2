39272e8b4b3600fc4c89193e23fb33b101eb8133
Charles Siu <chunheisiu@gmail.com>
2018-04-08 23:33:53 -0700

    Upload classification tree code

--- Charles.R
+++ Charles.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(magrittr)
 library(ISLR)
 library(tree)
+library(caret)
 
 setwd("/Users/chunheisiu/Dropbox/Documents/USF/2018_Spring/MATH_373/CaseStudy/CaseStudy2")
 
@@ -15,22 +16,44 @@ dropVars <- function(df) {
 }
 
 # Set up variables
+data.train <- na.omit(data.train)
+data.test <- na.omit(data.test)
 data.train <- dropVars(data.train)
 data.test <- dropVars(data.test)
-data.train$popular <- as.factor(data.train$popular)
-data.test$popular <- as.factor(data.test$popular)
+
+data.train$popular <- factor(data.train$popular)
+data.test$popular <- factor(data.test$popular)
+
 data.train.class <- data.train$popular
 data.test.class <- data.test$popular
-length_data.train <- dim(data.train)[1]
 
-data.train %<>% 
-  mutate(content = as.factor(ifelse(n_tokens_content <= 500, 1, 0)))
+# Upsampling the data since it is unbalanced towards 0
+set.seed(1)
+data.train.upsample <- upSample(data.train, data.train.class, T)$x
 
 # Classification tree
-tree.train <- tree(content ~ .-popular, data = data.train)
+tree.train <- tree(popular ~ ., data = data.train.upsample)
 summary(tree.train)
 
 plot(tree.train)
 text(tree.train, pretty = 0)
 
 tree.pred <- predict(tree.train, data.test, type = "class")
+table(tree.pred, data.test.class)
+mean(tree.pred == data.test.class)
+
+cv.pop <- cv.tree(tree.train, FUN = prune.misclass)
+cv.pop
+
+par(mfrow = c(1, 2))
+plot(cv.pop$size, cv.pop$dev, type = "b")
+plot(cv.pop$k, cv.pop$dev, type = "b")
+
+par(mfrow = c(1,1))
+prune.pop <- prune.misclass(tree.train, best = cv.pop$size[which.min(cv.pop$dev)])
+plot(prune.pop)
+text(prune.pop, pretty = 0)
+
+prune.pred <- predict(prune.pop, data.test, type = "class")
+table(prune.pred, data.test.class)
+mean(prune.pred == data.test.class)
